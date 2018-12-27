e8a1ed15d0f2527b9dd5238762ff865bfcceff6e
life <life@jwang11.com>
2018-12-27 17:56:00 +0800

    方法新加   @PostConstruct,作用方便让你看到缓存管理器的实例是自己定义的

--- src/main/java/com/wgg/cache/CacheApplication.java
+++ src/main/java/com/wgg/cache/CacheApplication.java
@@ -1,8 +1,12 @@
 package com.wgg.cache;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+
+import javax.annotation.PostConstruct;
 
 @EnableCaching
 @SpringBootApplication
@@ -12,5 +16,13 @@ public class CacheApplication {
         SpringApplication.run(CacheApplication.class, args);
     }
 
+
+    @Autowired
+    private CacheManager cacheManager;
+
+    @PostConstruct
+    public void post(){
+        System.out.println(cacheManager.getClass().getName());
+    }
 }
 
